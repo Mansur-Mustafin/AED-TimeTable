23b3a88bf3c1abf32d0c954057d147e5e432f94b
Ian <ianbeltrao@gmail.com>
2022-10-21 14:38:52 +0100

    changes to main and ignore test

--- main.cpp
+++ main.cpp
@@ -6,10 +6,23 @@ int main() {
 
     Operations op ("students_classes.csv", "classes_per_uc.csv","classes.csv");
 
-    vector<Student> v = op.students_in_year(2);
+    //op.print_time_table(op.GetTimeTable("202020897"));
+    //cout << op.N_of_students_in_UC(Subject("L.EIC001,xXXXXxx"));
+    //cout << op.N_of_students_in_class(Subject("L.EIC001,1LEIC01"));
+    //cout << op.N_of_students_in_year(2);
+    /*
+    vector<Student> v = op.students_in_class(Subject("L.EIC001,1LEIC01"));
     for(auto s : v){
         cout << s << endl;
     }
+    */
+    vector<Student> v = op.students_in_year(1);
+    for(auto s : v){
+        cout << s << endl;
+    }
+
+    //cout << a;
+
 
     return 0;
 }
